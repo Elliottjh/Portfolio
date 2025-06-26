c662c9c417c9ecb78db65a6afd8baa1cfc1d274d
Apple <apple@macbookpro.home>
2025-06-26 16:20:55 +0100

    Update URLs for Portfolio repository

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/building-portfolio">
+    <BrowserRouter basename="/Portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
